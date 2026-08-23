5f6de1656c680efa2632669c5b4731b3bcf33200
ozkanceng <ozkan39290@gmail.com>
2026-08-23 15:55:44 +0300

    DEV: rename deprecated ES6 helper extension

--- assets/javascripts/discourse/lib/fcm-notifications.js
+++ assets/javascripts/discourse/lib/fcm-notifications.js
@@ -3,12 +3,12 @@ import { ajax } from "discourse/lib/ajax";
 export function subscribe(subscription) {
   return ajax("/fcm_notifications/subscribe", {
     type: "POST",
-    data: { subscription: subscription }
+    data: { subscription },
   });
 }
 
 export function unsubscribe() {
   return ajax("/fcm_notifications/unsubscribe", {
-    type: "POST"
+    type: "POST",
   });
 }
